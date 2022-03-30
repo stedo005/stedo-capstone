3864e4824822028a34af37ac2d143a31fc24bbeb
Steve Dölgner <steve.doelgner@gmail.com>
2022-03-30 13:12:33 +0200

    setup for frontend

--- backend/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,15 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
+
+    @Value("${hello-cash.username}")
+    String username;
+    @Value("${hello-cash.password}")
+    String password;
 
 }
