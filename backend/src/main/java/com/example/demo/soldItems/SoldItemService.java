d78187b5acb86fe2cd007c75747946519160c8bf
Steve Dölgner <steve.doelgner@gmail.com>
2022-03-31 20:42:27 +0200

    unused imports

--- backend/src/main/java/com/example/demo/soldItems/SoldItemService.java
+++ backend/src/main/java/com/example/demo/soldItems/SoldItemService.java
@@ -6,8 +6,6 @@ import com.example.demo.helloCash.dataModel.HelloCashItem;
 import com.example.demo.user.UserData;
 import com.example.demo.user.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
