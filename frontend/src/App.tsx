b69843b1de0519a4f818d2cbcbfad3849b61356b
Steve Dölgner <steve.doelgner@gmail.com>
2022-03-31 14:56:46 +0200

    start implement login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
     return (
         <div>
+            <div>
+                Hallo
+            </div>
+            <Outlet />
         </div>
     );
 }
