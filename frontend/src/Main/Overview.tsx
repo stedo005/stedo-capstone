cda63f1ea7da02b91fc43109feba92d9daa2d336
Steve Dölgner <steve.doelgner@gmail.com>
2022-04-04 16:39:52 +0200

    input date added

--- frontend/src/Main/Overview.tsx
+++ frontend/src/Main/Overview.tsx
@@ -1,8 +1,12 @@
 import {useTranslation} from "react-i18next";
+import {useState} from "react";
 
 const Overview = () => {
 
     const {t} = useTranslation()
+
+    const [dateFrom, setDateFrom] = useState("")
+    const [dateTo, setDateTo] = useState("")
 
     const refreshDatabase = () => {
 
@@ -18,6 +22,10 @@ const Overview = () => {
     return (
         <div>
             <button onClick={refreshDatabase}>{t("Datenbank aktualisieren")}</button><br/><br/>
+            von: <input type={"date"} value={dateFrom} onChange={e => setDateFrom(e.target.value)}/><br/>
+            bis: <input type={"date"} value={dateTo} onChange={e => setDateTo(e.target.value)}/><br/>
+            {dateFrom}<br/>
+            {dateTo}
         </div>
     )
 
