e22703425a04329ec6e40e1fd941fbfe2b7ddd2d
Steve Dölgner <steve.doelgner@gmail.com>
2022-04-05 17:13:42 +0200

    frontend list

--- frontend/src/Main/EvaluateCategory.tsx
+++ frontend/src/Main/EvaluateCategory.tsx
@@ -1,16 +1,17 @@
-import {useNavigate, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import {useTranslation} from "react-i18next";
 import {useState} from "react";
+import {soldItem} from "../Models/model";
 
 
 const EvaluateCategory = () => {
 
     const linkedId = useParams()
     const {t} = useTranslation()
-    const navigate = useNavigate()
 
     const [dateFrom, setDateFrom] = useState("")
     const [dateTo, setDateTo] = useState("")
+    const [listOfItems, setListOfItems] = useState([] as soldItem[])
 
     const sendDate = () => {
 
@@ -26,6 +27,8 @@ const EvaluateCategory = () => {
                 "Authorization": "Bearer " + localStorage.getItem("token")
             }
         })
+            .then(response => {return response.json()})
+            .then((responseBody: soldItem[]) => setListOfItems(responseBody))
 
     }
 
@@ -36,6 +39,9 @@ const EvaluateCategory = () => {
             von: <input type={"date"} value={dateFrom} onChange={e => setDateFrom(e.target.value)}/><br/>
             bis: <input type={"date"} value={dateTo} onChange={e => setDateTo(e.target.value)}/><br/><br/>
             <button onClick={sendDate}>senden</button>
+            <div>
+                {listOfItems.map(e => <div>{e.itemName}</div>)}
+            </div>
 
         </div>
     )
