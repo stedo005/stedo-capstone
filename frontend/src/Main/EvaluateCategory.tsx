ceda6747a632a04978867257c98a894e8a5fdd46
Steve Dölgner <steve.doelgner@gmail.com>
2022-04-07 13:13:21 +0200

    calculations frontend

--- frontend/src/Main/EvaluateCategory.tsx
+++ frontend/src/Main/EvaluateCategory.tsx
@@ -13,6 +13,10 @@ const EvaluateCategory = () => {
     const [dateTo, setDateTo] = useState("2022-02-05")
     const [result, setResult] = useState(0)
     const [soldItems, setSoldItems] = useState([] as soldItem[])
+    const [hide, setHide] = useState(true)
+    const [calculationFactor, setCalculationFactor] = useState(2)
+    let budget = 1 / calculationFactor * result
+    let profit = result - budget
 
     const sendDate = () => {
 
@@ -44,11 +48,30 @@ const EvaluateCategory = () => {
             {t("Kategorie")} mit id: {linkedId.categoryId}<br/><br/>
             von: <input type={"date"} value={dateFrom} onChange={e => setDateFrom(e.target.value)}/> bis: <input type={"date"} value={dateTo} onChange={e => setDateTo(e.target.value)}/><br/><br/>
             <button onClick={sendDate}>senden</button><br/><br/>
-            <div>{t("Umsatz: ")}{result} €</div>
+            <div>
+                {t("Umsatz: ")}{result} €
+            </div>
+            <div>
+                <label htmlFor={"faktor"}>{t("Kalkulationsfaktor: ")}</label>
+                <input
+                    id={"faktor"}
+                    type={"number"}
+                    value={calculationFactor}
+                    onChange={e => setCalculationFactor(parseFloat(e.target.value))}
+                />
+            </div>
+            <div>
+                {t("Budget: ")}{budget}
+            </div>
+            <div>
+                {t("Gewinn: ")}{profit}
+            </div>
+            <br/>
+            <button onClick={() =>hide ? setHide(false) : setHide(true)}>{t("Artikelansicht an/aus")}</button><br/><br/>
             {
                 soldItems.length > 0
                 ?
-                    <div>{soldItems.map(e => <div>{e.itemName}</div>)}</div>
+                    <div hidden={hide}>{soldItems.map(e => <div>{e.itemName}</div>)}</div>
                 :
                     <div></div>
             }
