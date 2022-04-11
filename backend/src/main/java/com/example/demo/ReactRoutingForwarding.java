e8129437d85a208c096b2d804dd737e0a9a78ab3
Steve Dölgner <steve.doelgner@gmail.com>
2022-04-11 14:46:18 +0200

    try

--- backend/src/main/java/com/example/demo/ReactRoutingForwarding.java
+++ backend/src/main/java/com/example/demo/ReactRoutingForwarding.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ReactRoutingForwarding {
     @RequestMapping(value = "/**/{[path:[^\\.]*}")
     public String forwardToRoutUrl() {
-        return "forward:/overview";
+        return "forward:overview";
     }
 }
