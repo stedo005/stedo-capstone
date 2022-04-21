8617bb7d1a91caf0154d2aea7b97b47bd71c01f8
Steve Dölgner <steve.doelgner@gmail.com>
2022-04-21 15:47:56 +0200

    myDate function

--- frontend/src/Main/EvaluateCategory.tsx
+++ frontend/src/Main/EvaluateCategory.tsx
@@ -39,6 +39,27 @@ const EvaluateCategory = () => {
     let budget = 1 / calculationFactor * sumAll
     let profit = sumAll - budget
 
+    const myDate = (date: string) => {
+        let dateToFormat = new Date(date)
+        let weekday
+        if (dateToFormat.getDay() === 0) {
+            weekday = "So"
+        } else if (dateToFormat.getDay() === 1){
+            weekday = "Mo"
+        } else if (dateToFormat.getDay() === 2){
+            weekday = "Di"
+        } else if (dateToFormat.getDay() === 3){
+            weekday = "Mi"
+        } else if (dateToFormat.getDay() === 4){
+            weekday = "Do"
+        } else if (dateToFormat.getDay() === 5){
+            weekday = "Fr"
+        } else {
+            weekday = "Sa"
+        }
+        return  weekday + " " + dateToFormat.toLocaleDateString()
+    }
+
     const send = () => {
 
         fetch(`${process.env.REACT_APP_BASE_URL}/api/soldItems/evaluateCategory?searchTerm=${linkedId.categoryId}&dateFrom=${dateFrom}&dateTo=${dateTo}`, {
